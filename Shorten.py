3d12cfd2830e664b22e57e32a91eb92e803ad697
User <EMAIL>
2023-12-01 22:39:08 -0500

    Added Verification of input.
    Ensured that the start and end times are valid floats or ints.

--- Shorten.py
+++ Shorten.py
@@ -4,15 +4,26 @@ from moviepy.editor import *
 def cut_and_save_video():
     # Get user input for file name, start time, and end time
     file_name = input("Enter the video file name (including extension): ")
-    start_time = float(input("Enter the start time (in seconds): "))
-    end_time = float(input("Enter the end time (in seconds): "))
 
+    while True:
+        try:
+            start_time = float(input("Enter the START time (in seconds): "))
+            break  # Exit the loop if input is a valid number
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+    while True:
+        try:
+            end_time = float(input("Enter the END time (in seconds): "))
+            break  # Exit the loop if input is a valid number
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
     try:
         # Load the video clip
         video_clip = VideoFileClip(file_name)
 
         # Validate start and end times
-        if 0 <= start_time < end_time <= video_clip.duration:
+        if 0 <= start_time < end_time <= video_clip.duration: #If the startTime is greater than 0 & endTime is greater than start time and less than duration
             # Trim the video
             trimmed_clip = video_clip.subclip(start_time, end_time)
 
@@ -21,6 +32,7 @@ def cut_and_save_video():
             trimmed_clip.write_videofile(output_file, codec="libx264", audio_codec="aac")
 
             print(f"Video successfully trimmed and saved as {output_file}")
+            
         else:
             print("Invalid start or end time. Make sure they are within the video duration.")
 
